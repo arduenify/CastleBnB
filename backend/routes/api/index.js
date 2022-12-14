044d9310de649540df90a6e5d1bd59bf3879ecc0
Adam Scoggins <scoggins.aj@gmail.com>
2022-12-13 22:09:16 -0600

    Fixed the api index router by removing two nested routers that are unnecessary

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,16 +1,9 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
 
-const sessionRouter = require('./session');
-const usersRouter = require('./users');
-
 const { restoreUser } = require('../../utils/auth');
 
 router.use(restoreUser);
-
-router.use('/session', sessionRouter);
-
-router.use('/users', usersRouter);
 
 router.get('/test', function (req, res) {
     res.json({ message: 'Looks like the API is running.' });
