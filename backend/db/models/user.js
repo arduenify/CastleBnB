4b62f06dca7958fc57dc4218b50b8266ca53312a
Adam Scoggins (TC219) <scoggins.aj@gmail.com>
2022-12-15 16:14:43 -0600

    Added missing associations and validation

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -9,8 +9,8 @@ module.exports = (sequelize, DataTypes) => {
          * The `models/index` file will call this method automatically.
          */
         static associate(models) {
-            User.hasMany(models.Booking);
-            User.hasMany(models.Review);
+            User.hasMany(models.Booking, { foreignKey: 'userId' });
+            User.hasMany(models.Review, { foreignKey: 'userId' });
             User.hasMany(models.Spot, { foreignKey: 'ownerId' });
         }
     }
