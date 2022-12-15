114a7a9f45b80e29530127a7cb7ae2c018050fa2
Adam Scoggins (TC219) <scoggins.aj@gmail.com>
2022-12-15 15:47:20 -0600

    Added association's and validation for the Review table

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -1,5 +1,6 @@
 'use strict';
 const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
     class Review extends Model {
         /**
@@ -8,20 +9,35 @@ module.exports = (sequelize, DataTypes) => {
          * The `models/index` file will call this method automatically.
          */
         static associate(models) {
-            // define association here
+            Review.belongsTo(models.User, { foreignKey: 'userId' });
+            Review.belongsTo(models.Spot, { foreignKey: 'spotId' });
         }
     }
+
     Review.init(
         {
-            userId: DataTypes.INTEGER,
-            spotId: DataTypes.INTEGER,
-            review: DataTypes.STRING,
-            stars: DataTypes.INTEGER,
+            userId: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+            spotId: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+            review: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            stars: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
         },
         {
             sequelize,
             modelName: 'Review',
         }
     );
+
     return Review;
 };
