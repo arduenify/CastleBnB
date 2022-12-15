f45d7de200477e392e8055156f25e9148d1d842a
Adam Scoggins (TC219) <scoggins.aj@gmail.com>
2022-12-15 12:17:01 -0600

    Fixed formatting and fixed typo in schema

--- backend/db/migrations/20221214052808-create-spot.js
+++ backend/db/migrations/20221214052808-create-spot.js
@@ -1,55 +1,66 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Spots', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      ownerId: {
-        type: Sequelize.INTEGER
-      },
-      address: {
-        type: Sequelize.STRING
-      },
-      city: {
-        type: Sequelize.STRING
-      },
-      state: {
-        type: Sequelize.STRING
-      },
-      country: {
-        type: Sequelize.STRING
-      },
-      lat: {
-        type: Sequelize.NUMERIC
-      },
-      lng: {
-        type: Sequelize.NUMERIC
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      description: {
-        type: Sequelize.STRING
-      },
-      price: {
-        type: Sequelize.NUMERIC
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Spots');
-  }
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('Spots', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            ownerId: {
+                allowNull: false,
+                type: Sequelize.INTEGER,
+                references: { model: 'Users', key: 'id' },
+                onDelete: 'CASCADE',
+            },
+            address: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            city: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            state: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            country: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            lat: {
+                type: Sequelize.NUMERIC,
+            },
+            lng: {
+                type: Sequelize.NUMERIC,
+            },
+            name: {
+                type: Sequelize.STRING(50),
+            },
+            description: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            price: {
+                type: Sequelize.NUMERIC(0, 2),
+                allowNull: false,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+            },
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('Spots');
+    },
 };
