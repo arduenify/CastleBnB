a13743d4f426f23b95b126e6846d0f703e16224b
Adam Scoggins <scoggins.aj@gmail.com>
2023-01-02 17:30:29 -0600

    created create-schema migration for production testing

--- backend/db/migrations/0-create-schema.js
+++ backend/db/migrations/0-create-schema.js
@@ -5,8 +5,11 @@ module.exports = {
     async up(queryInterface, Sequelize) {
         const allSchemas = await queryInterface.sequelize.showAllSchemas();
 
+        console.log('AllSchemas: ', allSchemas);
+
         if (!allSchemas.includes('airbnb_clone')) {
-            await Sequelize.createSchema('airbnb_clone');
+            const result = await Sequelize.createSchema('airbnb_clone');
+            console.log('Create Schema Result: ', result);
         }
     },
 
