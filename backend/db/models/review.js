1b91ff7105597634c8893665f4cce51c1f14803c
Adam Scoggins <scoggins.aj@gmail.com>
2023-01-02 03:17:58 -0600

    Removed comments

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -3,11 +3,6 @@ const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
     class Review extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
         static associate(models) {
             Review.belongsTo(models.User, { foreignKey: 'userId' });
             Review.belongsTo(models.Spot, { foreignKey: 'spotId' });
--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -4,11 +4,6 @@ const bcrypt = require('bcryptjs');
 
 module.exports = (sequelize, DataTypes) => {
     class User extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
         static associate(models) {
             User.hasMany(models.Booking, { foreignKey: 'userId' });
             User.hasMany(models.Review, { foreignKey: 'userId' });
